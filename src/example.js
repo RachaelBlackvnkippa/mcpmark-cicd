302f157a35f2551df180db6aa955973615eab6fe
RachaelBlackvnkippa <braver_mopes_8l@icloud.com>
2025-12-12 10:39:09 +0800

    Fix linting errors in example.js

--- src/example.js
+++ src/example.js
@@ -2,6 +2,6 @@
 const unusedVariable = 'This variable is unused';
 console.log('Hello, world!');
 // Missing semicolon at end of line
-const anotherVariable = 'This variable is also unused'
+const anotherVariable = 'This variable is also unused';
 // Missing quotes around string
-const missingQuotes = This is a string without quotes;
+const missingQuotes = 'This is a string without quotes';
